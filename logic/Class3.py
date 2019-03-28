0ce668a1f5e3d0ca1e020c75d9b08a25059b18b8
varo <varog74@gmail.com>
2019-03-28 14:49:35 +0100

    Class 3 terminada Conversor a numeros romanos

--- logic/Class3.py
+++ logic/Class3.py
@@ -1,4 +1,4 @@
-while (True):
+"""while (True):
     arabi = int(input('Ingresa un número arábigo: '))
     if arabi>0 and arabi<31:
         x=(arabi//10)*'X'
@@ -15,4 +15,29 @@ while (True):
             v=(xs//5)*'V'
             print(x+v+i)
     else:
-        print('No podemos operar esos números')
+        print('No podemos operar esos números') """
+
+
+def numerosRomanos():
+    Vn = [100, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    Vc = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    NumeroRomano = []
+
+    while (True):
+        x = int(input("\n Ingresa un numero decimal / 0 terminar:"))
+
+        if (x == 0):
+            break
+
+        i = 0
+
+        while (x > 0):
+            if (x >= Vn[i]):
+                NumeroRomano.append(Vc[i])
+                x = x - Vn[i]
+            else:
+                i = i + 1
+        print(str(x), "=>", *NumeroRomano)
+
+
+numerosRomanos()
